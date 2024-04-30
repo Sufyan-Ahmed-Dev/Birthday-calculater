ea753b422dce1068026e2e117d2e75c9998029f9
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-04-30 08:47:10 +0500

    bhirthday cal aproximately Done in console

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-let date = new Date()
-let DateOfBirth = new Date(2004,10,3)
-console.log(DateOfBirth.toLocaleDateString())
+let today = new Date();
 
-let age = date.getFullYear() - DateOfBirth.getFullYear()
-console.log(`you are ${age} Years Old`)
+let birthdate = new Date(2004,10,2);
 
-let nextBirthDayDate = new Date(date.getFullYear() ,DateOfBirth.getMonth(), DateOfBirth.getDate())
-// console.log(nextBirthDayDate);
+let nextBirthday = new Date(today.getFullYear(), birthdate.getMonth(), birthdate.getDate());
+if (nextBirthday < today) {
+    nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
+}
 
-console.log(nextBirthDayDate.toLocaleDateString())
-let nextBirthDay_DaysRemain = Math.ceil((nextBirthDayDate.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
+const timeRemain = nextBirthday.getTime() - today.getTime();
+//  Time 
+let months = Math.floor(timeRemain / (1000 * 60 * 60 * 24 * 30 ));
+let days = Math.floor(timeRemain %  (1000 * 60 * 60 * 24 * 30 ) / (1000 * 60 * 60 * 24));
+let hours = Math.floor((timeRemain % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((timeRemain % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((timeRemain % (1000 * 60)) / 1000);
+// console.table([months , days,hours,minutes, seconds])
 
-console.log(nextBirthDay_DaysRemain)
+let result = `Time remaining until your next birthday:${months} months, ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
+
+
+console.log(result)
