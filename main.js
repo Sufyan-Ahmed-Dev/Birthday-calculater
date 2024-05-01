337a66dc110d748442685b3388508a7bd7bdd41a
Sufyan Ahmed <sufiyan033130@gmail.com>
2024-05-01 22:09:56 +0500

    user to console

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
-let today = new Date();
+let form = document.querySelector("#form");
+let inputValue = document.querySelector("#userInput");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  inputValue = inputValue.value;
+  console.log(inputValue);
 
-let birthdate = new Date(2004,10,2);
+  let today = new Date();
 
-let nextBirthday = new Date(today.getFullYear(), birthdate.getMonth(), birthdate.getDate());
-if (nextBirthday < today) {
+  let birthdate = new Date(inputValue);
+
+  let nextBirthday = new Date(
+    today.getFullYear(),
+    birthdate.getMonth(),
+    birthdate.getDate()
+  );
+  if (nextBirthday < today) {
     nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
-}
+  }
 
-const timeRemain = nextBirthday.getTime() - today.getTime();
-//  Time 
-let months = Math.floor(timeRemain / (1000 * 60 * 60 * 24 * 30 ));
-let days = Math.floor(timeRemain %  (1000 * 60 * 60 * 24 * 30 ) / (1000 * 60 * 60 * 24));
-let hours = Math.floor((timeRemain % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-let minutes = Math.floor((timeRemain % (1000 * 60 * 60)) / (1000 * 60));
-let seconds = Math.floor((timeRemain % (1000 * 60)) / 1000);
-// console.table([months , days,hours,minutes, seconds])
+  const timeRemain = nextBirthday.getTime() - today.getTime();
+  //  Time
+  let months = Math.floor(timeRemain / (1000 * 60 * 60 * 24 * 30));
+  let days = Math.floor(
+    (timeRemain % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+  );
+  let hours = Math.floor(
+    (timeRemain % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutes = Math.floor((timeRemain % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeRemain % (1000 * 60)) / 1000);
+  // console.table([months , days,hours,minutes, seconds])
 
-let result = `Time remaining until your next birthday:${months} months, ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
+  let result = `Time remaining until your next birthday:${months} months, ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds.`;
 
-
-console.log(result)
+  console.log(result);
+});
